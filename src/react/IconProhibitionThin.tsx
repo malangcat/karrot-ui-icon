35e96046083fec3855e2855fe34fc970cd61f549
Hyeseong Kim <tim@daangn.com>
2022-07-29 16:00:50 +0900

    update icons

---
+++ src/react/IconProhibitionThin.tsx
@@ -0,0 +1,24 @@
+import { SVGProps } from 'react';
+
+const IconProhibitionThin = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    data-karrot-ui-icon="true"
+    {...props}
+  >
+    <path
+      d="M5.57574 5.42426C5.81005 5.18995 6.18995 5.18995 6.42426 5.42426L18.8553 17.8553C19.0896 18.0896 19.0897 18.4695 18.8553 18.7039C18.621 18.9382 18.2411 18.9382 18.0068 18.7039L5.57574 6.27279C5.34142 6.03848 5.34142 5.65858 5.57574 5.42426Z"
+      fill="currentColor"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 3.33333C7.21353 3.33333 3.33333 7.21353 3.33333 12C3.33333 16.7865 7.21353 20.6667 12 20.6667C16.7865 20.6667 20.6667 16.7865 20.6667 12C20.6667 7.21353 16.7865 3.33333 12 3.33333Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default IconProhibitionThin;
